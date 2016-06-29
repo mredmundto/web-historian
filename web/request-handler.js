dda3d685dc2932a688a735f25ed1230da91bb202
mredmundto <yatlunto@gmail.com>
2016-06-29 15:45:36 -0700

    Implement handle request function for get request
    Pass testing case for empty url, a specified url and non-existing url

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,31 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs'); 
+var http = require('http'); 
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  
+  var statusCode; 
+  if (req.method === 'GET' && req.url === '/') {
+    fs.readFile(__dirname + '/public/index.html', function (error, data) {
+      res.end(data);
+    });
+  }
+
+  if (req.method === 'GET' && req.url !== '/') { 
+    fs.readFile(archive.paths.archivedSites + req.url, function (error, data) {
+      if (data === undefined) {
+        statusCode = 404; 
+        res.writeHead(statusCode);
+        res.end(); 
+      } else {
+        res.end(data);
+      }
+    });
+  }
+
+
+
+  // res.end(archive.paths.list);
 };
