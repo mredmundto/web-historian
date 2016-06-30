956f38b212d580389074054e3235bb19debea764
mredmundto <yatlunto@gmail.com>
2016-06-29 20:44:58 -0700

    Completed archive

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,13 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var request = require('request');
+var archive = require('../helpers/archive-helpers');
+
+var fetch = function(){
+  console.log('this is fetch!!!')
+
+  archive.readListOfUrls(function(arr){
+    archive.downloadUrls(arr);
+  });
+};
+fetch()
