bd786845e04e9ae4e05d623555e20eb0e03cc0bd
mredmundto <yatlunto@gmail.com>
2016-06-29 21:58:12 -0700

    loading an archive page problem

--- web/request-handler.js
+++ web/request-handler.js
@@ -16,17 +16,34 @@ exports.handleRequest = function (req, res) {
     });
     req.on('end', function() {
       // write the url the file
+      url = body.split('url=')[1]; 
+      // this line in the isURLArchived????
       fs.appendFile(archive.paths.list, body.split('url=')[1] + '\n', function() {
         
-        console.log(archive.paths.siteAssets + '/loading.html');
-
-        fs.readFile(archive.paths.siteAssets + '/loading.html', function(error, data) {
-          statusCode = '302'; 
-          res.writeHead(statusCode);
-          res.write(data); 
-          res.end();
+        archive.isUrlArchived(url, function(exists) {
+          if (exists) {
+            fs.readFile(archive.paths.archivedSites + '/' + url, function(error, data) {
+              statusCode = '302'; 
+              res.writeHead(statusCode);
+              res.write(data); 
+              res.end();
+            });
+          } else {
+            fs.readFile(archive.paths.siteAssets + '/loading.html', function(error, data) {
+              statusCode = '302'; 
+              res.writeHead(statusCode);
+              res.write(data); 
+              res.end();
+            });
+          }
         });
-         
+        /*
+        // Will this still run?????
+        console.log(archive.paths.siteAssets + '/loading.html');
+          }
+      // if url is not archive, redirect to loading.html  
+        
+        */ 
       });
       //viewing the file
       //var fileContents = fs.readFileSync(archive.paths.list, 'utf8');
